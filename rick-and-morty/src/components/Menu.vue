d47bd58d54618e36c6d41e560036fdeff20cb1d1
Marcos Alves <gaspaulo_mgoo@hotmail.com>
2024-07-12 17:27:24 -0300

    update menu

---
+++ rick-and-morty/src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <img src="../assets/svg/rick.png" alt="Logo">
+    <ul>
+      <li><a href="#about">About</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style scoped>
+/* Adicione aqui o CSS para o seu componente */
+</style>
